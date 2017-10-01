8d3176242f1f9614ae822e924d9e112c94bff891
Toby Hamand <tobyhamand@gmail.com>
2017-10-01 17:01:39 +0100

    added extra tests

--- spec/primeCalculatorSpec.js
+++ spec/primeCalculatorSpec.js
@@ -6,7 +6,13 @@ describe("PrimeCalculator", function() {
   describe("#calculate", function(){
     it("returns 'yes' if the number is a prime", function(){
       var prime = 7;
+      var bigprime = 1553;
       expect(primeCalculator.calculate(prime)).toEqual('yes');
+      expect(primeCalculator.calculate(bigprime)).toEqual('yes');
+    });
+    it("returns '' if the number is not a prime", function(){
+      var prime = 4;
+      expect(primeCalculator.calculate(prime)).toEqual('');
     });
   });
 });
--- spec/wordCounterSpec.js
+++ spec/wordCounterSpec.js
@@ -4,7 +4,7 @@ describe("WordCounter", function() {
   });
 
   describe("#count", function(){
-    it("counts the individual words in an array", function(){
+    it("counts the individual words in an array, returning a hash of results", function(){
       var wordArray = ['foo', 'foo', 'bar'];
       expect(wordCounter.count(wordArray)).toEqual({ 'foo' : 2, 'bar' : 1});
     });
